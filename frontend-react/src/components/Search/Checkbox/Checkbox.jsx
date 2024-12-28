ba267464c94ac8ee49f63f402964414d574d5387
Igor <igoroshust75@gmail.com>
2024-12-28 20:37:33 +0900

    feat: add checkbox component (search)

---
+++ frontend-react/src/components/Search/Checkbox/Checkbox.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Checkbox = ({ checkboxState, handleCheckboxChange }) => {
+
+    const labels = {
+        maxCompletenessSign: 'Признак максимальной полноты',
+        businessContextMentions: 'Упоминания в бизнес-контексте',
+        leadingRole: 'Главная роль в публикации',
+        riskFactorsPublicationsOnly: 'Публикации только с риск факторами',
+        addTechnicalNews: 'Включать технические новости рынков',
+        addAnnouncements: 'Включать анонсы и календари',
+        addNewsBulletins: 'Включать сводки новостей',
+    };
+
+    return (
+        <div className="search-form__checkboxes">
+            <div className="search-form__checkboxes__column">
+                {Object.keys(checkboxState).map((key) => (
+                    <div key={key} className="check">
+                        <input
+                         type="checkbox"
+                         className="checkbox"
+                         id={`checkbox${key}`}
+                         name={key}
+                         onChange={handleCheckboxChange}
+                         checked={checkboxState[key]}
+                         />
+
+                        <label className="label" for={`checkbox${key}`}>
+                            <span className="checkmark"></span>
+                            <span className="label-text paragraph-18">{ labels[key] }</span>
+                        </label>
+                    </div>
+                ))}
+            </div>
+       </div>
+    );
+};
+
+export default Checkbox;
--- frontend-react/src/components/Search/Search.jsx
+++ frontend-react/src/components/Search/Search.jsx
@@ -10,13 +10,14 @@ const Search = () => {
     const [startDate, setStartDate] = useState('');
     const [endDate, setEndDate] = useState('');
     const [checkboxState, setCheckhoxState] = useState({
-        maxCompleteness: false,
-        businessMentions: false,
-        mainRole: false,
-        riskFactorsOnly: false,
-        includeMarketNews: true,
-        includeAnnouncements: true,
-        includeNewsSummaries: true,
+
+        maxCompletenessSign: false,
+        businessContextMentions: false,
+        leadingRole: false,
+        riskFactorsPublicationsOnly: false,
+        addTechnicalNews: true,
+        addAnnouncements: true,
+        addNewsBulletins: true,
     });
 
     // Объявляем зависимости
@@ -105,7 +106,9 @@ const Search = () => {
               excludeDigests: !checkboxState.includeNewsSummaries,
             },
 
-            limit: Number(documentCount), // Максимальное количество возвращаемых документов
+            // Максимальное количество возвращаемых документов
+            limit: Number(documentCount),
+
             // Сортировка и группировка результатов
             sortType: "sourceInfluence",
             sortDirectionType: "desc",
@@ -120,3 +123,143 @@ const Search = () => {
           console.log('Форма не валидна, перенаправление не выполнено.');
         }
 };
+
+        return(
+            <section class="search">
+                <div class="container">
+                    <div class="search__row">
+                                <form class="search-form" onSubmit={handleSubmit}>
+                                        <div class="search-form__row">
+
+                                            {/* Колонка инпутов */}
+                                            <div class="search-form__inputs">
+
+                                                <div class="search-form__section search-form__section_item-1">
+                                                    <label class="paragraph-18" for="inn">ИНН компании<span class="paragraph-25-500">*</span></label><br>
+                                                    <input class="search-form__input paragraph-14" placeholder="10 цифр" maxlength="10" type="text" id="inn" name="inn">
+                                                </div>
+
+                                                <div class="search-form__section search-form__section_item-2">
+                                                    <label class="paragraph-18" for="tonality">Тональность</label><br>
+                                                    <select class="search-form__select-big search-form__select-big_item-1" id="tonality">
+                                                        <option class="search-form__select-big_option search-form__select-big_option-0" value="Любая">Любая</option>
+                                                        <option class="search-form__select-big_option search-form__select-big_option-1" value="Значение-1">Значение 1</option>
+                                                        <option class="search-form__select-big_option search-form__select-big_option-2" value="Значение-2">Значение 2</option>
+                                                        <option class="search-form__select-big_option search-form__select-big_option-3" value="Значение-3">Значение 3</option>
+                                                    </select>
+                                                </div>
+
+                                                <div class="search-form__section search-form__section_item-3">
+                                                    <label class="paragraph-18" for="documents_count">Количество документов в выдаче<span class="paragraph-25-500">*</span></label><br>
+                                                    <input class="search-form__input paragraph-14" placeholder="От 1 до 1000" maxlength="4" type="text" id="documents_count" name="documents_count">
+                                                </div>
+
+                                                <div class="search-form__section search-form__section_item-4">
+                                                    <label class="paragraph-18" for="searchSelect-small-1">Диапазон поиска<span class="paragraph-25-500">*</span></label><br>
+                                                    <div class="search-form_section__row">
+                                                        <select class="search-form__select-small search-form__select-small_item-1 paragraph-14" id="searchSelect-small-1">
+                                                            <option class="search-form__select-small_option search-form__select-small_option-1" value="Дата начала">Дата начала</option>
+                                                            <option class="search-form__select-small_option search-form__select-small_option-2" value="Значение 1">Значение 1</option>
+                                                            <option class="search-form__select-small_option search-form__select-small_option-3" value="Значение 2">Значение 2</option>
+                                                            <option class="search-form__select-small_option search-form__select-small_option-4" value="Значение 3">Значение 3</option>
+                                                        </select>
+
+                                                        <select class="search-form__select-small search-form__select-small_item-2 paragraph-14" id="searchSelect-small-2">
+                                                            <option class="search-form__select-small_option search-form__select-small_option-0" value="Дата конца">Дата конца</option>
+                                                            <option class="search-form__select-small_option search-form__select-small_option-1" value="Значение 1">Значение 1</option>
+                                                            <option class="search-form__select-small_option search-form__select-small_option-2" value="Значение 2">Значение 2</option>
+                                                            <option class="search-form__select-small_option search-form__select-small_option-3" value="Значение 3">Значение 3</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <!-- Колонка чек-боксов -->
+                                            <div class="search-form__checkboxes">
+                                                <div class="search-form__checkboxes__column">
+                                                    <!-- Оборачиваем checkbox в label для увеличения области кликабельности -->
+
+                                                    <div class="check check-0">
+                                                        <input type="checkbox" name="option0" value="option0" class="checkbox" id="checkbox0" checked>
+                                                        <label class="label" for="checkbox0">
+                                                            <span class="checkmark"></span>
+                                                            <span class="label-text paragraph-18">Признак максимальной полноты</span>
+                                                        </label>
+                                                    </div>
+
+                                                    <div class="check check-1">
+                                                        <input type="checkbox" name="option1" value="option1" class="checkbox" id="checkbox1" checked>
+                                                        <label class="label" for="checkbox1">
+                                                            <span class="checkmark"></span>
+                                                            <span class="label-text paragraph-18">Упоминания в бизнес-контексте</span>
+                                                        </label>
+                                                    </div>
+
+                                                    <div class="check check-2">
+                                                        <input type="checkbox" name="option2" value="option2" class="checkbox" id="checkbox2" checked>
+                                                        <label class="label" for="checkbox2">
+                                                            <span class="checkmark"></span>
+                                                            <span class="label-text paragraph-18">Главная роль в публикации</span>
+                                                        </label>
+                                                    </div>
+
+                                                    <div class="check check-3">
+                                                        <input type="checkbox" name="option3" value="option3" class="checkbox" id="checkbox3">
+                                                        <label class="label" for="checkbox3">
+                                                            <span class="checkmark"></span>
+                                                            <span class="label-text paragraph-18">Публикации только с риск-факторами</span>
+                                                        </label>
+                                                    </div>
+
+                                                    <div class="check check-4">
+                                                        <input type="checkbox" name="option4" value="option4" class="checkbox" id="checkbox4">
+                                                        <label class="label" for="checkbox4">
+                                                            <span class="checkmark"></span>
+                                                            <span class="label-text paragraph-18">Включать технические новости рынков и всякое такое там ещё текст какой-то</span>
+                                                        </label>
+                                                    </div>
+
+                                                    <div class="check check-5">
+                                                        <input type="checkbox" name="option5" value="option5" class="checkbox" id="checkbox5" checked>
+                                                        <label class="label" for="checkbox5">
+                                                            <span class="checkmark"></span>
+                                                            <span class="label-text paragraph-18">Включать анонсы и календари</span>
+                                                        </label>
+                                                    </div>
+
+                                                    <div class="check check-6">
+                                                        <input type="checkbox" name="option6" value="option6" class="checkbox" id="checkbox6">
+                                                        <label class="label" for="checkbox6">
+                                                            <span class="checkmark"></span>
+                                                            <span class="label-text paragraph-18">Включать сводки новостей</span>
+                                                        </label>
+                                                    </div>
+                                                </div>
+                                              </div>
+
+                                            <!-- Кнопка "Поиск" -->
+                                            <div class="search-button__button">
+                                                    <button class="btn auth-btn__btn-login search-btn paragraph-22-500" name="searchButton" disabled>Поиск</button>
+                                                        <div class="search-paragraph-container">
+                                                            <p class="search-paragraph paragraph-14-300">* Обязательные к заполнению поля</p>
+                                                        </div>
+                                                  </div>
+                                        </div>
+                                </form>
+                            </div>
+
+                            <div class="search__secret-img none">
+                                <img src="./img/search/man-search.png" alt="СКАН поиск данных">
+                            </div>
+                        </div>
+
+                        <div class="search__img">
+                            <img src="./img/search/man-search.png" alt="СКАН поиск данных">
+                        </div>
+                    </div>
+                </div>
+    </section>
+        );
+};
+
+export default Search;
